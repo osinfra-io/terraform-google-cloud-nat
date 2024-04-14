ae5b64fbc3ad153a29871440b49c55a6b5425414
Brett Curtis <brett@osinfra.io>
2024-04-14 18:53:35 -0400

    Deprecation of Kitchen-Terraform Fixes #17

--- fixtures/list_of_subnets/main.tf
+++ fixtures/list_of_subnets/main.tf
@@ -1,11 +1,11 @@
 module "test" {
-  source = "../../../regional"
+  source = "../../regional"
 
   # Cloud NAT (network address translation) lets certain resources without external IP addresses create outbound connections to the internet.
   # https://cloud.google.com/nat/docs/overview
 
-  network                            = "kitchen-vpc"
-  prefix                             = "kitchen-vpc-subnetworks"
+  network                            = "terraform-test-vpc"
+  prefix                             = "terraform-test-vpc-subnetworks"
   project                            = var.project
   region                             = "us-east1"
   source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
